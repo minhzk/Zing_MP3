ea4dbfc70960f49b84bccc2863419be0844f26c6
Nguyễn Bình Minh <nbinhminh158@gmail.com>
2023-12-06 19:05:46 +0700

    setting react router dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Home, Login, Public} from './containers/public'
+import { Routes, Route } from 'react-router-dom'
+import path from './utils/path';
+
 function App() {
-  const { test, homeData } = useSelector(state => state.app)
-  console.log(test, homeData);
   return (
     <>
       <div className=''>
-        app
+        <Routes>
+          <Route path={path.PUBLIC} element={<Public />}>
+            <Route path={path.HOME} element={<Home />} />
+            <Route path={path.LOGIN} element={<Login />} />
+
+          </Route>
+        </Routes>
       </div>
       <ToastContainer
       position="top-right"
--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import reduxConfig from './redux';
+import {
+  BrowserRouter
+} from "react-router-dom";
 
 const store = reduxConfig()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
