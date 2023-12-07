1c7a22a5707f3daafecee941281a8402735d5521
Nguyễn Bình Minh <nbinhminh158@gmail.com>
2023-12-07 23:51:31 +0700

    feat: set up slider

--- src/containers/public/Home.js
+++ src/containers/public/Home.js
@@ -1,23 +1,14 @@
 import React, { useEffect } from 'react'
-import { Header } from '../../components'
-import * as apis from '../../apis'
+import { Header, Slider } from '../../components'
 
 const Home = () => {
-
-  useEffect(() => {
-    const fetchDataHome = async() => {
-      const res = await apis.getHome()
-      console.log(res);
-    }
-
-    fetchDataHome()
-  }, [])
 
   return (
     <div className='overflow-y-auto px-[59px]'>
       <div className='h-[70px] flex items-center'>
         <Header/>
       </div>
+      <Slider/>
 
     </div>
   )
