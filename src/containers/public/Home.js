701f68649ef04530929613a06b0dff1cece672d5
Nguyễn Bình Minh <nbinhminh158@gmail.com>
2023-12-07 22:13:28 +0700

    feat: set up APIs

--- src/containers/public/Home.js
+++ src/containers/public/Home.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Header } from '../../components'
+import * as apis from '../../apis'
 
 const Home = () => {
+
+  useEffect(() => {
+    const fetchDataHome = async() => {
+      const res = await apis.getHome()
+      console.log(res);
+    }
+
+    fetchDataHome()
+  }, [])
+
   return (
     <div className='overflow-y-auto px-[59px]'>
       <div className='h-[70px] flex items-center'>
         <Header/>
       </div>
+
     </div>
   )
 }
